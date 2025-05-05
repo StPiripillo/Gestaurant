aa86d1616329c8eb60d32e89857aaa60ffb5c9a4
beatricedurante <beatricedurante00@gmail.com>
2025-05-05 16:54:57 +0200

    c

--- src/main/java/enums/intolleranze.java
+++ src/main/java/enums/intolleranze.java
@@ -1,4 +1,8 @@
 package enums;
 
 public enum intolleranze {
+    GLUTINE,
+    LATTOSIO,
+    SOLFITI,
+    CONSERVANTI
 }
--- src/main/java/enums/tipologia.java
+++ src/main/java/enums/tipologia.java
@@ -1,4 +1,10 @@
 package enums;
 
 public enum tipologia {
+    PRIMI,
+    SECONDI,
+    ANTIPASTI,
+    CONTORNI,
+    BEVANDE,
+    DOLCI
 }
