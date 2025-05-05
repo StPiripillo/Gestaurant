c6ad74abf7a0ad3ef34f283d32751bd6194cbe18
Piripillo <savinoclaudio29@gmail.com>
2025-05-05 15:23:39 +0200

    Versione palla

---
+++ src/main/java/org/example/gestaurant/GestaurantApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gestaurant/GestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
