f2320f02eb60a5bb06117ec21222dd91614ca4bd
Silvestro21 <21silvestrorinaldi@gmail.com>
2025-05-05 17:00:52 +0200

    V0 entity prodotto,sedia

--- src/main/java/models/prodotto.java
+++ src/main/java/models/prodotto.java
@@ -1,4 +1,27 @@
 package models;
 
-public class prodotto {
+import enums.intolleranze;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class prodotto extends baseEntity {
+
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	@Enumerated
+	private intolleranze intolleranze;
+	@OneToMany(mappedBy = "prodotto", fetch = FetchType.EAGER)
+	private List<ingredienti> ingredienti;
 }
--- src/main/java/models/sedia.java
+++ src/main/java/models/sedia.java
@@ -1,4 +1,19 @@
 package models;
 
-public class sedia {
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class sedia extends tavolo {
+
+	private boolean occupata;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private tavolo tavolo;
 }
