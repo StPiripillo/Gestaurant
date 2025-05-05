a27e575355d7d94c51021000f2d498b0baca7710
Silvestro21 <21silvestrorinaldi@gmail.com>
2025-05-05 17:57:57 +0200

    Dto e controller iniziale

---
+++ src/main/java/controllers/TavoloController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import dao.ordineDao;
+import dao.tavoloDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tavolo")
+public class TavoloController
+{
+	@Autowired
+	private tavoloDao tavoloDao;
+	@Autowired
+	private ordineDao ordineDao;
+
+
+
+}
