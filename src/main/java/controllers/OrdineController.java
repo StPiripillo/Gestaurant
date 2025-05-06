0f9500a3cd64a1267ca464cf5481d7fa6826bf2e
PietroRM <pietromirto@gmail.com>
2025-05-06 15:03:33 +0200

    List getall prodotti

--- src/main/java/controllers/OrdineController.java
+++ src/main/java/controllers/OrdineController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import dao.ordineDao;
 import dto.OrdineDTO;
 import dto.services.OrdineService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +8,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/ordine")
-public class OrdineController
-{
+public class OrdineController {
 	@Autowired
 	private OrdineService ordineService;
 
+	@Autowired
+	private ordineDao oDao;
+
 	@GetMapping
-	public List<OrdineDTO> getAll()
-	{
-		return ordineService.getAll();
+	public List<OrdineDTO> getAll() {
+
+		return oDao.findAll().stream().map(ordine -> new OrdineDTO(
+						ordine.getId(),
+						ordine.getProdotti(),
+						ordine.getTotale()
+				))
+				.collect(Collectors.toList());
+
 	}
+
+
 }
