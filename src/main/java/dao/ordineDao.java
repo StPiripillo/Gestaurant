131a105c3fdbeb92f1a2a2b278b66c98a8eca652
PietroRM <pietromirto@gmail.com>
2025-05-06 15:04:49 +0200

    List getall prodotti

--- src/main/java/dao/ordineDao.java
+++ src/main/java/dao/ordineDao.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Id;
 import models.ordine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
-
 public interface ordineDao extends JpaRepository<Long, ordine>
 {
 	ordine findByIdOrdine(Long idOrdine);
