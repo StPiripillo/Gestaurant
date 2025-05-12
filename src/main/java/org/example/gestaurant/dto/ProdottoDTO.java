38daa8db1a562fa8b0176b42c8f92d1e6f3263c3
Silvestro21 <21silvestrorinaldi@gmail.com>
2025-05-12 10:04:39 +0200

    Enum aggiustato

--- src/main/java/org/example/gestaurant/dto/ProdottoDTO.java
+++ src/main/java/org/example/gestaurant/dto/ProdottoDTO.java
@@ -1,5 +1,6 @@
 package org.example.gestaurant.dto;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.example.gestaurant.enums.Intolleranze;
 import org.example.gestaurant.enums.Tipologia;
 
